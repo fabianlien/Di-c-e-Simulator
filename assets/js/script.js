4eeed9ae35b69e18f220352b4f556226b462c2da
Fabian Lien <fabian.lien@gmail.com>
2022-03-07 15:49:16 +0000

    add function to increment sides on selected die by 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,18 +13,27 @@ function summonDice(numDice) {
     }
     
     document.getElementsByClassName("setting-die")[0].addEventListener("click", function() {
-        this.style.color = "orange";
+        dieSides(this);
     })
     document.getElementById("setting-dice-container").lastChild.addEventListener("click", function() {
-        this.style.color = "orange";
+        dieSides(this);
     })
 
     displaySum();
 }
 
 /** Lets the user select the amount of sides to each die */
-function dieSides() {
-
+function dieSides(dieIndex) {
+    let diceTypes = [`<i class="fa-solid fa-dice-two"></i>`, `<i class="fa-solid fa-dice-three"></i>`, `<i class="fa-solid fa-dice-four"></i>`, `<i class="fa-solid fa-dice-five"></i>`, `<i class="fa-solid fa-dice-six"></i>`, `<i class="fa-solid fa-dice-d20"></i>7`, `<i class="fa-solid fa-dice-d20"></i>8`, `<i class="fa-solid fa-dice-d20"></i>9`, `<i class="fa-solid fa-dice-d20"></i>10`, `<i class="fa-solid fa-dice-d20"></i>11`, `<i class="fa-solid fa-dice-d20"></i>12`, `<i class="fa-solid fa-dice-d20"></i>13`, `<i class="fa-solid fa-dice-d20"></i>14`, `<i class="fa-solid fa-dice-d20"></i>15`, `<i class="fa-solid fa-dice-d20"></i>16`, `<i class="fa-solid fa-dice-d20"></i>17`, `<i class="fa-solid fa-dice-d20"></i>18`, `<i class="fa-solid fa-dice-d20"></i>19`, `<i class="fa-solid fa-dice-d20"></i>20`]
+    dieIndex.outerHTML = 
+    `<div id="caret-container">
+        <div id="increase-btn"><i class="fa-solid fa-caret-up"></i></div>
+        <span id="active-setting-die" class="setting-die">${diceTypes[i]}</span>
+        <div id="decrease-btn"><i class="fa-solid fa-caret-down"></i></div>
+    </div>`;
+    document.getElementById("increase-btn").addEventListener("click", function () {
+      document.getElementById("active-setting-die").innerHTML = diceTypes[i++];
+    })
 }
 
 
